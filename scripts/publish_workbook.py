df6f5cb071b51bf606c581d6fac95b074967b63f
niravp1 <110545460+niravp1@users.noreply.github.com>
2022-10-28 16:18:52 +0530

    s
    s

--- scripts/publish_workbook.py
+++ scripts/publish_workbook.py
@@ -12,9 +12,9 @@ def main(args):
         server = TSC.Server(args.server_url)
         project_data_json = project_data['workbooks']
         with server.auth.sign_in(tableau_auth):
-            site_item = server.sites.get_by_name('Enterprise')
+            site_item = server.sites.get_by_name('DataLab')
             print(site_item.id, site_item.name, site_item.content_url, site_item.state)
-            
+
             try:
                 for data in project_data_json:
                     wb_path = os.path.dirname(os.path.realpath(__file__)).rsplit(
