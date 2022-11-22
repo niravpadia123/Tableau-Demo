d22dfa2f3e06bb712daa1c7b1d8fae8f7bc7b05f
niravp1 <110545460+niravp1@users.noreply.github.com>
2022-11-22 17:25:57 +0530

    code to publish wb in prod
    code to publish wb in prod

--- scripts/publish.py
+++ scripts/publish.py
@@ -10,22 +10,23 @@ def publish_wb(server, data):
     """
     Funcrion Description
     """
-    project_id = get_project_id(server, data['project_path'], data['file_path'])
+    project_id = get_project_id(
+        server, data['publish_wb_data']['project_path'], data['publish_wb_data']['file_path'])
     wb_path = os.path.dirname(os.path.realpath(__file__)).rsplit(
-        '/', 1)[0] + "/workbooks/" + data['file_path']
+        '/', 1)[0] + "/workbooks/" + data['publish_wb_data']['file_path']
 
     new_workbook = TSC.WorkbookItem(
-        name=data['wb_name'], project_id=project_id, show_tabs=data['show_tabs'])
+        name=data['publish_wb_data']['wb_name'], project_id=project_id, show_tabs=data['publish_wb_data']['show_tabs'])
     new_workbook = server.workbooks.publish(
-        new_workbook, wb_path, "Overwrite", hidden_views=data['hidden_views']
-        if len(data['hidden_views']) > 0 else None)
+        new_workbook, wb_path, "Overwrite", hidden_views=data['publish_wb_data']['hidden_views']
+        if len(data['publish_wb_data']['hidden_views']) > 0 else None)
 
     print(
-        f"\nSuccessfully published {data['file_path']} Workbook in {data['project_path']} project in {data['site_name']} site.")
+        f"\nSuccessfully published {data['publish_wb_data']['file_path']} Workbook in {data['publish_wb_data']['project_path']} project in {data['publish_wb_data']['site_name']} site.")
 
     # Update Workbook and set tags
-    if len(data['tags']) > 0:
-        new_workbook.tags = set(data['tags'])
+    if len(data['publish_wb_data']['tags']) > 0:
+        new_workbook.tags = set(data['publish_wb_data']['tags'])
         new_workbook = server.workbooks.update(
             new_workbook)
         print("\nUpdate Workbook Successfully and set Tags.")
@@ -37,7 +38,10 @@ def publish_ds(server, data, dl_ds_file_path):
     """
     Funcrion Description
     """
-    project_id = get_project_id(server, data['datasource']['publish_ds_project_name'], data['datasource']['ds_name'])
+    project_id = get_project_id(
+        server, data['datasource']['publish_ds_data']['publish_ds_project_name'],
+        data['datasource']['ds_name']
+    )
 
     # Use the project id to create new datsource_item
     new_datasource = TSC.DatasourceItem(project_id)
@@ -47,6 +51,6 @@ def publish_ds(server, data, dl_ds_file_path):
         new_datasource, dl_ds_file_path, 'Overwrite')
 
     print(
-        f"\nSuccessfully published {data['datasource']['ds_name']} datasource in {data['datasource']['publish_ds_project_name']} in {data['site_name']} site.")
+        f"\nSuccessfully published {data['datasource']['ds_name']} datasource in {data['datasource']['publish_ds_data']['publish_ds_project_name']} in {data['datasource']['publish_ds_data']['publish_ds_site_name']} site.")
 
     return new_datasource._id
