3393b749e972b881277a2e50d5b877b4fb48c5a4
niravp1 <110545460+niravp1@users.noreply.github.com>
2022-11-22 11:45:54 +0530

    new code and publish data
    new code and publish data

--- scripts/service.py
+++ scripts/service.py
@@ -11,81 +11,79 @@ def temp_func(data, username, password):
     """
     Funcrion Description
     """
+    server, auth_token, version = sign_in(
+        username, password, data['server_url'], data['site_name'], data['is_site_default'])
+
+    # Publish Workbook Part
     try:
-        if data['project_path']:
-            server, auth_token, version = sign_in(
-                username, password, data['server_url'], data['site_name'], data['is_site_default'])
-
-            # Step: Form a new workbook item and publish.
+        # Step: Form a new workbook item and publish.
+        if data['is_wb_publish']:
             wb_id = publish_wb(server, data)
+    except Exception as tableu_exception:
+        logging.error(
+            "Something went wrong in publish workbook.\n %s", tableu_exception)
 
-            if len(data['permissions']) > 0:
-                for permission_data in data['permissions']:
-                    if permission_data['permission_template']:
-                        is_group = None
+    # Permissions Part
+    try:
+        if data['is_wb_permissions_update']:
+            for permission_data in data['permissions']:
+                is_group = None
 
-                        # Step: Get the User or Group ID of permission assigned
-                        if permission_data['permission_group_name'] and \
-                                not permission_data['permission_user_name']:
-                            permission_user_or_group_id = get_group_id(
-                                server, permission_data['permission_group_name'])[0]
-                            is_group = True
-                        elif not permission_data['permission_group_name'] and \
-                                permission_data['permission_user_name']:
-                            permission_user_or_group_id = get_user_id(
-                                server, permission_data['permission_user_name'])[0]
-                            is_group = False
-                        else:
-                            logging.info(
-                                "permission_group_name and permission_user_name are both null, Please provide anyone of that.")
+                # Step: Get the User or Group ID of permission assigned
+                if permission_data['permission_group_name'] and \
+                        not permission_data['permission_user_name']:
+                    permission_user_or_group_id = get_group_id(
+                        server, permission_data['permission_group_name'])[0]
+                    is_group = True
+                elif not permission_data['permission_group_name'] and \
+                        permission_data['permission_user_name']:
+                    permission_user_or_group_id = get_user_id(
+                        server, permission_data['permission_user_name'])[0]
+                    is_group = False
 
-                        # get permissions of specific workbook
-                        user_permissions = query_permission(
-                            data, wb_id, permission_user_or_group_id,
-                            version, auth_token, is_group)
+                # get permissions of specific workbook
+                user_permissions = query_permission(
+                    data, wb_id, permission_user_or_group_id,
+                    version, auth_token, is_group)
 
-                        for permission_name, permission_mode in \
-                                permission_data['permission_template'].items():
-                            if user_permissions is None:
+                for permission_name, permission_mode in \
+                        permission_data['permission_template'].items():
+                    if user_permissions is None:
+                        add_permission(
+                            data, wb_id, permission_user_or_group_id, version,
+                            auth_token, permission_name, permission_mode, is_group)
+                        print(
+                            f"\tPermission {permission_name} is set to {permission_mode} Successfully in {wb_id}\n")
+                    else:
+                        for permission in user_permissions:
+                            if permission.get('name') == permission_name and \
+                                    permission.get('mode') != permission_mode:
+                                existing_mode = permission.get(
+                                    'mode')
+
+                                delete_permission(
+                                    data, auth_token, wb_id,
+                                    permission_user_or_group_id, permission_name,
+                                    existing_mode, version, is_group)
+                                print(
+                                    f"\tPermission {permission_name} : {existing_mode} is deleted Successfully in {wb_id}\n")
+
                                 add_permission(
-                                    data, wb_id, permission_user_or_group_id, version,
-                                    auth_token, permission_name, permission_mode, is_group)
+                                    data, wb_id, permission_user_or_group_id,
+                                    version, auth_token, permission_name,
+                                    permission_mode, is_group)
                                 print(
                                     f"\tPermission {permission_name} is set to {permission_mode} Successfully in {wb_id}\n")
-                            else:
-                                for permission in user_permissions:
-                                    if permission.get('name') == permission_name and \
-                                            permission.get('mode') != permission_mode:
-                                        existing_mode = permission.get(
-                                            'mode')
+    except Exception as tableu_exception:
+        logging.error(
+            "Something went wrong in update permission of workbook.\n %s", tableu_exception)
 
-                                        delete_permission(
-                                            data, auth_token, wb_id,
-                                            permission_user_or_group_id, permission_name,
-                                            existing_mode, version, is_group)
-                                        print(
-                                            f"\tPermission {permission_name} : {existing_mode} is deleted Successfully in {wb_id}\n")
+    # Step: Sign Out to the Tableau Server
+    server.auth.sign_out()
 
-                                        add_permission(
-                                            data, wb_id, permission_user_or_group_id,
-                                            version, auth_token, permission_name,
-                                            permission_mode, is_group)
-                                        print(
-                                            f"\tPermission {permission_name} is set to {permission_mode} Successfully in {wb_id}\n")
-                    else:
-                        logging.info(
-                            "Something went wrong, Error occured.\n User Name or List of Permissions in template are null")
-            else:
-                logging.info(
-                    "Something went wrong, Error occured.\n Permissions in template are null")
-
-            # Step: Sign Out to the Tableau Server
-            server.auth.sign_out()
-
-        if data['datasource'] and data['datasource']['ds_name'] \
-                and data['datasource']['get_ds_project_name'] \
-                and data['datasource']['publish_ds_project_name']:
-
+    # Datasource Part
+    try:
+        if data['is_datasource_update']:
             # Step: Sign In to the Tableau Server
             server, auth_token, version = sign_in(
                 username, password, data['datasource']['get_ds_server_url'], '', True)
@@ -107,15 +105,12 @@ def temp_func(data, username, password):
             # Publish Datasource
             publish_ds(server, data, dl_ds_file_path)
 
-            if data['datasource']['is_ds_refresh']:
-                # Refresh Datasource
-                ds_refresh(server, data['datasource']['ds_name'],
-                        data['datasource']['publish_ds_project_name'])
+            # Refresh Datasource
+            ds_refresh(server, data['datasource']['ds_name'],
+                       data['datasource']['publish_ds_project_name'])
 
             # Step: Sign Out to the Tableau Server
             server.auth.sign_out()
-
     except Exception as tableu_exception:
         logging.error(
-            "Something went wrong, Exception Fired.\n %s", tableu_exception)
-        exit(1)
+            "Something went wrong in datasource update.\n %s", tableu_exception)
