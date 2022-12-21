9ed467ddaffe9d983e1c6ad1abf5e3f0432527fd
nirav-ih <nirav.padia@invitationhomes.com>
2022-12-21 14:44:31 +0530

    fb
    fb

--- scripts/service.py
+++ scripts/service.py
@@ -100,7 +100,8 @@ def service_func(data, username, password, prod_username, prod_password, mpd):
 
     # Permissions Part
     try:
-        if data['is_wb_permissions_update']:
+        if data['is_wb_permissions_update'] and \
+                mpd[data['index_id']]['_is_' + data['publish_wb_data']['wb_name'] + '_published']:
             for permission_data in data['permissions']:
                 is_group = None
 
